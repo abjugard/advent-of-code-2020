b3b6ddef0ebe253644fcd32c588366b497a00e3d
Adrian Bjugård <adrian.bjugard@omegapoint.se>
2020-12-09 07:07:43 +0100

    Day 9: Optimised XMAS cracking

--- src/day09-encoding_error.py
+++ src/day09-encoding_error.py
@@ -4,36 +4,35 @@ from itertools import combinations
 today = day(2020, 9)
 
 
-def find_xmas_magic(inp):
-  for i in range(25, len(inp)):
-    combs = combinations(inp[i - 25:i], 2)
-    if not any(sum(comb) == inp[i] for comb in combs):
-      return inp[i]
+def find_xmas_magic(xmas_data):
+  for i in range(25, len(xmas_data)):
+    preceeding_pairs = combinations(xmas_data[i - 25:i], 2)
+    if all(sum(pair) != xmas_data[i] for pair in preceeding_pairs):
+      return xmas_data[i]
 
 
-def crack_xmas(magic_number, inp):
-  for i in range(len(inp)):
-    s = [inp[i]]
-    for j in range(i + 1, len(inp)):
-      s += [inp[j]]
-      if sum(s) == magic_number:
-        return min(s) + max(s)
-      if sum(s) > magic_number:
+def crack_xmas(magic_number, xmas_data):
+  chunk_start = 0
+  while xmas_data[chunk_start] < magic_number:
+    chunk = [xmas_data[chunk_start]]
+    for next_val in xmas_data[chunk_start + 1:]:
+      chunk.append(next_val)
+      if sum(chunk) == magic_number:
+        return min(chunk) + max(chunk)
+      if sum(chunk) > magic_number:
+        skip = 1
+        while sum(chunk[:skip + 1]) <= next_val:
+          skip += 1
+        chunk_start += skip
         break
 
 
-def parse(line):
-  return line
-
-
 def main():
-  inp = list(get_data(today, base_ops + [
-    ('func', int)
-  ], groups=False))
-  magic_number = find_xmas_magic(inp)
+  xmas_data = list(get_data(today, [('func', int)]))
+  magic_number = find_xmas_magic(xmas_data)
   print(f'{today} star 1 = {magic_number}')
-  print(f'{today} star 2 = {crack_xmas(magic_number, inp)}')
+  print(f'{today} star 2 = {crack_xmas(magic_number, xmas_data)}')
 
 
 if __name__ == '__main__':
-  timed(main)
+  bench(main)
