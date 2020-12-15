d9d7b67996164218d2fa0bd4f45bbf6c62318c13
Adrian Bjugård <adrian.bjugard@omegapoint.se>
2020-12-15 14:35:15 +0100

    Day 15: Faster

--- src/day15-rambunctious_recitation.py
+++ src/day15-rambunctious_recitation.py
@@ -3,25 +3,22 @@ from santas_little_helpers import *
 today = day(2020, 15)
 
 
-def play_game(init_rounds, target_turns):
+def play_game(init_rounds, game_length):
   seen = {n: i for i, n in enumerate(init_rounds)}
   last = 0
 
-  for turn in range(len(init_rounds), max(target_turns)):
+  for turn in range(len(init_rounds), game_length - 1):
     n = turn - seen[last] if last in seen else 0
-
-    if turn + 1 in target_turns:
-      yield last
-
     seen[last] = turn
     last = n
+
+  return last
 
 
 def main():
   init_rounds = next(get_data(today, [('split', ','), ('map', int)]))
-  star = play_game(init_rounds, [2020, 30000000])
-  print(f'{today} star 1 = {next(star)}')
-  print(f'{today} star 2 = {next(star)}')
+  print(f'{today} star 1 = {play_game(init_rounds, 2020)}')
+  print(f'{today} star 2 = {play_game(init_rounds, 30000000)}')
 
 
 if __name__ == '__main__':
