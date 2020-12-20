7f4cf0bb9f17e1ae4fd8395965ccc2129754b0e7
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2020-12-20 12:19:56 +0100

    Day 20: Both parts and cleanup

--- src/day20-jurassic_jigsaw.py
+++ src/day20-jurassic_jigsaw.py
@@ -1,72 +1,46 @@
 from santas_little_helpers import *
 from math import sqrt
-from copy import deepcopy
 
 today = day(2020, 20)
 
 
-def get_edges(grid):
-  return [
-    grid[0],
-    [grid[i][-1] for i in range(len(grid))],
-    grid[-1],
-    [grid[i][0] for i in range(len(grid))]
-  ]
+def rotate(grid): return [list(reversed(row)) for row in zip(*grid)]
+def flip(grid): return [list(reversed(row)) for row in grid]
 
 
-def rotate(edges):
-  return [
-    edges[1],
-    list(reversed(edges[2])),
-    edges[3],
-    list(reversed(edges[0]))
-  ]
-
-
-def flip(edges):
-  return [
-    list(reversed(edges[0])),
-    edges[3],
-    list(reversed(edges[2])),
-    edges[1],
-  ]
+def edges(g):
+  rows = list(range(len(g)))
+  return [g[0], [g[i][-1] for i in rows], g[-1], [g[i][0] for i in rows]]
 
 
 def alternatives(grid):
-  edges = get_edges(grid)
-  e = edges.copy()
   for _ in range(4):
-    e = rotate(e)
-    yield e
-  e = flip(edges.copy())
+    grid = rotate(grid)
+    yield edges(grid), grid
+  grid = flip(grid)
   for _ in range(4):
-    e = rotate(e)
-    yield e
+    grid = rotate(grid)
+    yield edges(grid), grid
 
 
 def matches(conditions, edges):
   return all(edges[cond] == other for cond, other in conditions.items())
 
 
-def candidates(consumed=set(), conditions=dict()):
-  for tile, grid in grids.items():
+def candidates(consumed, conditions):
+  for tile, alternatives in grids.items():
     if tile in consumed:
       continue
-    for alt in alternatives(grid):
+    for alt, g in alternatives:
       if matches(conditions, alt):
-        yield tile, alt
+        yield tile, alt, g
 
 
-def fill_empty(image, consumed):
-  target = None
-  for y in range(height):
-    for x in range(width):
-      if image[y][x] is None:
-        target = (x, y)
-        break
-    if target is not None:
+def fill_empty(image, consumed=set()):
+  for x, y in all_coords(width, height):
+    if image[y][x] is None:
       break
-  if target is None:
+  else:
     return image
   conditions = {}
   if x > 0:
@@ -74,26 +48,58 @@ def fill_empty(image, consumed):
   if y > 0:
     conditions[0] = image[y - 1][x]['edges'][2]
   n_image = image.copy()
-  n_image[y] = deepcopy(image[y])
-  for tile, edges in candidates(consumed, conditions):
-    n_image[y][x] = {'tile': tile, 'edges': edges}
+  n_image[y] = image[y].copy()
+  for tile, edges, g in candidates(consumed, conditions):
+    n_image[y][x] = {'tile': tile, 'edges': edges, 'grid': g}
     result = fill_empty(n_image, consumed | {tile})
     if result is not None:
       return result
   return None
 
 
-def part1():
-  found = fill_empty([[None for _ in range(width)] for _ in range(height)], set())
-  count = found[0][0]['tile']
-  count *= found[0][-1]['tile']
-  count *= found[-1][0]['tile']
-  count *= found[-1][-1]['tile']
-  return count
+def trim(grid):
+  return [row[1:-1] for row in grid[1:-1]]
 
 
-def part2(grids):
-  print(grids)
+def reconstruct_image(image_data):
+  trimmed = [[trim(item['grid']) for item in row] for row in image_data]
+  t_h, t_w = len(trimmed[0][0]), len(trimmed[0][0][0])
+  image = [[] for _ in range(height * t_w)]
+  for y, grids in enumerate(trimmed):
+    for yd, line in [t for grid in grids for t in enumerate(grid)]:
+      image[y * t_h + yd].extend(line)
+  return image
+
+
+def lay_puzzle():
+  image_data = [[None for _ in range(width)] for _ in range(height)]
+  image_data = fill_empty(image_data)
+
+  checksum = image_data[0][0]['tile']
+  checksum *= image_data[0][-1]['tile']
+  checksum *= image_data[-1][0]['tile']
+  checksum *= image_data[-1][-1]['tile']
+
+  return checksum, reconstruct_image(image_data)
+
+
+def find_monsters(image, monster):
+  m_h, m_w = len(monster), len(monster[0])
+  subgrid_h, subgrid_w = len(image) - m_h, len(image[0]) - m_w
+  return sum(all(image[y + m_y][x + m_x] == '#' or monster[m_y][m_x] == ' '
+                 for m_x, m_y in all_coords(m_w, m_h))
+             for x, y in all_coords(subgrid_w, subgrid_h))
+
+
+def habitat_rougness(image):
+  sea_monster = ['                  # ',
+                 '#    ##    ##    ###',
+                 ' #  #  #  #  #  #   ']
+  for _, monster in alternatives(sea_monster):
+    result = find_monsters(image, monster)
+    if result > 0:
+      break
+  return str(image).count('#') - str(sea_monster).count('#') * result
 
 
 def parse(group):
@@ -101,21 +107,16 @@ def parse(group):
   tile = int(lines[0][5:-1])
   grid = lines[1:]
   grid = [[grid[y][x] for x in range(len(grid[0]))] for y in range(len(grid))]
-  return tile, grid
+  return tile, list(alternatives(grid))
 
 
 def main():
   global width, height, grids
-  grids = dict(get_data(today, [
-    ('func', parse)
-  ], groups=True))
+  grids = dict(get_data(today, [('func', parse)], groups=True))
   width = height = int(sqrt(len(grids)))
-  star1 = part1()
-  print(f'{today} star 1 = {star1}')
-  # submit_answer(today, star1)
-  # star2 = part2(grids)
-  # print(f'{today} star 2 = {star2}')
-  # submit_answer(today, star2, 2)
+  checksum, image = lay_puzzle()
+  print(f'{today} star 1 = {checksum}')
+  print(f'{today} star 2 = {habitat_rougness(image)}')
 
 
 if __name__ == '__main__':
