6db22370caa68441d4f91e6f79e006150bf6ea21
Adrian Bjugård <adrian.bjugard@omegapoint.se>
2020-12-21 08:06:21 +0100

    Day 21: Cleaned up

--- src/day21-allergen_assessment.py
+++ src/day21-allergen_assessment.py
@@ -4,10 +4,6 @@ from collections import defaultdict
 today = day(2020, 21)
 
 
-def present(a_foods, i_foods):
-  return all(a_food in i_foods for a_food in a_foods)
-
-
 def find_inert_ingredients(foods):
   ingredient_map = defaultdict(set)
   allergen_map = defaultdict(set)
@@ -20,10 +16,11 @@ def find_inert_ingredients(foods):
   non_allergen_used = 0
   inert_ingredients = set()
   for ingredient, i_foods in ingredient_map.items():
-    if any(present(a_foods, i_foods) for a_foods in allergen_map.values()):
+    if any(a_foods < i_foods for a_foods in allergen_map.values()):
       continue
     inert_ingredients.add(ingredient)
     non_allergen_used += len(i_foods)
+
   return non_allergen_used, inert_ingredients
 
 
@@ -40,19 +37,16 @@ def dangerous_list(foods, inert_ingredients):
   while any(len(allergens) > 1 for allergens in alls.values()):
     for allergen, ingredients in alls.items():
       if len(ingredients) == 1:
-        for other in alls.keys():
-          if other == allergen:
-            continue
-          alls[other] -= ingredients
         dangerous[allergen] = next(iter(ingredients))
+        for other in alls.keys():
+          alls[other].discard(dangerous[allergen])
+
   return ','.join(ingredient for _, ingredient in sorted(dangerous.items()))
 
 
 def parse(line):
   ingredients, allergens = line.split(' (contains ')
-  ingredients = ingredients.split(' ')
-  allergens = allergens[:-1].split(', ')
-  return set(ingredients), set(allergens)
+  return set(ingredients.split(' ')), set(allergens[:-1].split(', '))
 
 
 def main():
