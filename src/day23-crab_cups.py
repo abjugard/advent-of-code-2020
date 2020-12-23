ec5635df871faf2ebcaf1f7c1729d52f4a7866fe
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2020-12-23 09:27:23 +0100

    Day 23: Added some operator shenaningans, even slower, but it looks pretty! :)

--- src/day23-crab_cups.py
+++ src/day23-crab_cups.py
@@ -10,22 +10,29 @@ class Cup:
     self.label = label
     self.n_cup = n_cup
 
+  def __matmul__(self, ns):
+    target = self.n_cup
+    while ns > 1:
+      target = target.n_cup
+      ns -= 1
+    return target
+
 
 def get_target(current, chain_pointer, limit):
   illegal = set()
   for _ in range(3):
     illegal.add(chain_pointer.label)
-    chain_pointer = chain_pointer.n_cup
+    chain_pointer @= 1
   target = limit if current.label == 1 else current.label - 1
   while target in illegal:
     target = limit if target == 1 else target - 1
   return jump[target]
 
 
-def splice(target, chain_pointer, chain_end):
-  original_n = target.n_cup
+def splice(target, chain_pointer):
+  original_n = target @ 1
   target.n_cup = chain_pointer
-  chain_end.n_cup = original_n
+  (chain_pointer @ 2).n_cup = original_n
 
 
 def play_game(init_order, cup_count=10, iterations=100):
@@ -39,29 +46,28 @@ def play_game(init_order, cup_count=10, iterations=100):
 
   current = cups[0]
   for _ in range(iterations):
-    chain_pointer = current.n_cup
-    chain_end = chain_pointer.n_cup.n_cup
-    current.n_cup = chain_end.n_cup
+    chain_pointer = current @ 1
+    current.n_cup = chain_pointer @ 3
 
     target = get_target(current, chain_pointer, cup_count)
-    splice(target, chain_pointer, chain_end)
+    splice(target, chain_pointer)
 
-    current = current.n_cup
+    current @= 1
   return jump[1]
 
 
 def play_short_game(init_order):
   cup = play_game(init_order, len(init_order))
   result = ''
-  while cup.n_cup.label != 1:
-    cup = cup.n_cup
+  while (cup @ 1).label != 1:
+    cup @= 1
     result += str(cup.label)
   return result
 
 
 def play_long_game(init_order):
   cup_one = play_game(init_order, 1_000_000, 10_000_000)
-  return cup_one.n_cup.label * cup_one.n_cup.n_cup.label
+  return (cup_one @ 1).label * (cup_one @ 2).label
 
 
 def main():
